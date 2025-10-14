74a4a43cb239ec57ea000f95cbdba332584468f4
kapilkushwah950 <kapilkushwah950@gmail.com>
2025-10-14 18:36:04 +0530

    13-10-2025

--- example10.java
+++ example10.java
@@ -4,6 +4,7 @@ public class example10 { public static void main(String[] args) {
    int a = 100;
     System.out.println(a+50);
 
-    String s = 100
+    String s = "100";
+    System.out.println(s+50);
 }
 }
--- example8.java
+++ example8.java
@@ -2,8 +2,8 @@ package practice1;
 
 public class example8 {
     public static void main(String[] args) {
-        String s = "This Is a Test String";
+        String s = "This Is a Test string";
         System.out.println(s.replace("test","best"));
-
+        System.out.println(s.replace("test","best"));
     }
 }
